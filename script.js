af750f952801d15202af614b07be85c603b6cf09
Amaanath Mumtaz <amaanathmumtaz@gmail.com>
2021-01-22 21:12:52 +0530

    Completed Fetching Data + Updating HTML from recieved Data

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+window.onload = () => {
+    RandomUserGenerator()
+}
+
+const RandomUserGenerator = () => {
+    fetch('https://randomuser.me/api')
+    .then((res) => {
+        return res.json()
+    }).then((data) => {
+        showRandomUserData(data)
+    })
+}
+
+showRandomUserData = (randomUser) => {
+    console.log(randomUser)
+
+    document.getElementById('name').innerText = `${randomUser.results[0].name.title} ${randomUser.results[0].name.first} ${randomUser.results[0].name.last}`
+
+    document.getElementById('email').innerText = `${randomUser.results[0].email}`
+
+
+
+    
+}

5692e198b60f4a07a9ee84ad7f3bbc8601952a9c
Amaanath Mumtaz <amaanathmumtaz@gmail.com>
2021-01-22 22:39:36 +0530

    Completed the App with added information of Random User + Added a small fontawesome icon 😉

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 window.onload = () => {
-    RandomUserGenerator()
+    randomUserGenerator()
 }
 
-const RandomUserGenerator = () => {
+const randomUserGenerator = () => {
     fetch('https://randomuser.me/api')
     .then((res) => {
         return res.json()
     }).then((data) => {
+        console.log(data)
         showRandomUserData(data)
     })
 }
 
 showRandomUserData = (randomUser) => {
-    console.log(randomUser)
+
+    if (randomUser.results[0].gender === "male") {
+        document.getElementById('gender').innerHTML = "<i class='fas fa-mars'></i>"
+    } else {
+        document.getElementById('gender').innerHTML = "<i class='fas fa-venus'></i>"
+    }
 
     document.getElementById('name').innerText = `${randomUser.results[0].name.title} ${randomUser.results[0].name.first} ${randomUser.results[0].name.last}`
 
+    document.getElementById('age').innerText = `${randomUser.results[0].dob.age}`
+
     document.getElementById('email').innerText = `${randomUser.results[0].email}`
 
-
+    document.getElementById('phone').innerText = `${randomUser.results[0].phone}`
 
     
 }
